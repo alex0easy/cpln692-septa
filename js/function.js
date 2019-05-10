888c25f32cab859c31f6df2f1d571833bbec3d12
YUE_GUO <alex.g.easy@gmail.com>
2019-05-09 22:17:40 -0400

    Zhuang Xiu 1.0

--- js/function.js
+++ js/function.js
@@ -12,19 +12,19 @@ var plotstations = function(list) {
 
 // Plotting and removing bus stops and routes without adding click functions
 var plotdots = function(list) {
-  _.each(list, function(o){
+  _.each(list, function(o) {
     o.addTo(map);
   });
 };
 
 var removedots = function(list) {
-  _.each(list, function(o){
+  _.each(list, function(o) {
     map.removeLayer(o);
   });
 };
 
 var plotroutes = function(list_gj, list_layer, style) {
-  _.each(list_gj, function(o){
+  _.each(list_gj, function(o) {
     list_layer.push(L.geoJSON(o, style));
   });
   plotdots(list_layer);
@@ -53,9 +53,58 @@ var checkandremove = function(layer) {
 // and display the results to the sidebar.
 var findwithin = function(area) {
 
-// Finding and plotting stops
-  trolleystopswithin = turf.pointsWithinPolygon(Trolley_Stop, area);
-  busstopswithin = turf.pointsWithinPolygon(Bus_Stop, area);
+  // Finding and plotting stops
+  var trolleystopslist = turf.pointsWithinPolygon(Trolley_Stop, area);
+  var busstopslist = turf.pointsWithinPolygon(Bus_Stop, area);
+
+  trolleystopswithin = trolleystopslist.features;
+  busstopswithin = busstopslist.features;
+
+  // Filter stops by days of service; Drop them if they have no service.
+  if ($('#weekday_select').is(':checked') == true) {
+    for (var i = 0; i < trolleystopswithin.length; i++) {
+      if (trolleystopswithin[i].properties.Weekday_To == 0) {
+        trolleystopswithin.splice(i, 1);
+        i--;
+      };
+    };
+    for (var i = 0; i < busstopswithin.length; i++) {
+      if (busstopswithin[i].properties.Weekday_To == 0) {
+        busstopswithin.splice(i, 1);
+        i--;
+      };
+    };
+  };
+
+  if ($('#saturday_select').is(':checked') == true) {
+    for (var i = 0; i < trolleystopswithin.length; i++) {
+      if (trolleystopswithin[i].properties.Saturday_T == 0) {
+        trolleystopswithin.splice(i, 1);
+        i--;
+      };
+    };
+    for (var i = 0; i < busstopswithin.length; i++) {
+      if (busstopswithin[i].properties.Saturday_T == 0) {
+        busstopswithin.splice(i, 1);
+        i--;
+      };
+    };
+  };
+
+  if ($('#sunday_select').is(':checked') == true) {
+    for (var i = 0; i < trolleystopswithin.length; i++) {
+      if (trolleystopswithin[i].properties.Sunday_Tot == 0) {
+        trolleystopswithin.splice(i, 1);
+        i--;
+      };
+    };
+    for (var i = 0; i < busstopswithin.length; i++) {
+      if (busstopswithin[i].properties.Sunday_Tot == 0) {
+        busstopswithin.splice(i, 1);
+        i--;
+      };
+    };
+  };
 
   removedots(trolleystopswithin_layer);
   trolleystopswithin_layer = [];
@@ -63,15 +112,15 @@ var findwithin = function(area) {
   removedots(busstopswithin_layer);
   busstopswithin_layer = [];
 
-  if (trolleystopswithin.features.length > 0) {
-    _.each(trolleystopswithin.features, function(stop) {
+  if (trolleystopswithin.length > 0) {
+    _.each(trolleystopswithin, function(stop) {
       var marker = L.circleMarker([stop.properties.Latitude, stop.properties.Longitude], TrolleySStyle);
       trolleystopswithin_layer.push(marker);
     });
   };
 
-  if (busstopswithin.features.length > 0) {
-    _.each(busstopswithin.features, function(stop) {
+  if (busstopswithin.length > 0) {
+    _.each(busstopswithin, function(stop) {
       var marker = L.circleMarker([stop.properties.Latitude, stop.properties.Longitude], BusSStyle);
       busstopswithin_layer.push(marker);
     });
@@ -80,13 +129,13 @@ var findwithin = function(area) {
   plotdots(trolleystopswithin_layer);
   plotdots(busstopswithin_layer);
 
-// Finding and plotting routes
+  // Finding and plotting routes
   var dirtytrolleylist = [];
   var dirtybuslist = [];
-  _.each(trolleystopswithin.features, function(stop) {
+  _.each(trolleystopswithin, function(stop) {
     dirtytrolleylist.push(stop.properties.Route);
   });
-  _.each(busstopswithin.features, function(stop) {
+  _.each(busstopswithin, function(stop) {
     if (stop.properties.Route != " ") {
       dirtybuslist.push(stop.properties.Route);
     };
@@ -103,14 +152,14 @@ var findwithin = function(area) {
   busrouteswithin_gj = [];
   busrouteswithin_layer = [];
 
-  _.each(Trolley_Route.features, function(route){
-    if (trolleyrouteswithin.includes(route.properties.Route) == true){
+  _.each(Trolley_Route.features, function(route) {
+    if (trolleyrouteswithin.includes(route.properties.Route) == true) {
       trolleyrouteswithin_gj.push(route);
     };
   });
 
-  _.each(Bus_Route.features, function(route){
-    if (busrouteswithin.includes(route.properties.Route) == true){
+  _.each(Bus_Route.features, function(route) {
+    if (busrouteswithin.includes(route.properties.Route) == true) {
       busrouteswithin_gj.push(route);
     };
   });
@@ -118,15 +167,15 @@ var findwithin = function(area) {
   plotroutes(busrouteswithin_gj, busrouteswithin_layer, BusRStyle);
   plotroutes(trolleyrouteswithin_gj, trolleyrouteswithin_layer, TrolleyRStyle);
 
-// Changes text in the tally box
+  // Changes text in the tally box
   changeresult();
-// Generates the boxes
+  // Generates the boxes
   generatebox('#trolleyboxes', trolleyrouteswithin_gj, "trolleybox");
   generatebox('#busboxes', busrouteswithin_gj, 'busbox');
 };
 
 // Changes text in result box.
-var changeresult = function(){
+var changeresult = function() {
   $('#notrolley').text(trolleyrouteswithin.length + " trolley");
   $('#nobus').text(busrouteswithin.length + " bus");
   $('#searchresulttally').show();
@@ -134,45 +183,45 @@ var changeresult = function(){
 };
 
 // Generates the result boxes we see.
-var generatebox = function(where, list, style){
+var generatebox = function(where, list, style) {
   $(where).empty();
-  _.each(list, function(route){
+  _.each(list, function(route) {
     var boxcontent;
-    var settextstyle = function (style) {
-      if (style=="trolleybox") {
+    var settextstyle = function(style) {
+      if (style == "trolleybox") {
         return "trolleywords";
       } else {
         return "buswords";
       };
     };
-    
+
     var textstyle = settextstyle(style);
-    boxcontent = '<div class=' + style + '><p class='+ textstyle +'><a href="#" data-id='+route.properties.Route+' id="R'+route.properties.Route+'">'+route.properties.Route + '</a><br></p><p class='+ textstyle +'notbold>' + route.properties.Route_Name + '</p></div>';
+    boxcontent = '<div class=' + style + '><p class=' + textstyle + '><a href="#" data-id=' + route.properties.Route + ' id="R' + route.properties.Route + '">' + route.properties.Route + '</a><br></p><p class=' + textstyle + 'notbold>' + route.properties.Route_Name + '</p></div>';
 
     $(where).append(boxcontent);
   });
 
-  _.each(list, function(route){
+  _.each(list, function(route) {
     var id = '#R' + route.properties.Route
-    $(id).on('click', function () {
-      routetobehighlighted= $(this).data('id');
+    $(id).on('click', function() {
+      routetobehighlighted = $(this).data('id');
       highlightroute(routetobehighlighted);
     });
   });
 };
 
 // Highlights selected route on click.
-var highlightroute = function (rnumber){
+var highlightroute = function(rnumber) {
   checkandremove(highlighted_layer);
-  highlighted_layer_gj=[];
+  highlighted_layer_gj = [];
 
-  _.each(trolleyrouteswithin_gj, function(route){
+  _.each(trolleyrouteswithin_gj, function(route) {
     if (route.properties.Route == rnumber) {
       highlighted_layer_gj.push(route);
     };
   });
 
-  _.each(busrouteswithin_gj, function(route){
+  _.each(busrouteswithin_gj, function(route) {
     if (route.properties.Route == rnumber) {
       highlighted_layer_gj.push(route);
     };
@@ -180,7 +229,6 @@ var highlightroute = function (rnumber){
 
   highlighted_layer = L.geoJSON(highlighted_layer_gj, HighlightStyle);
   highlighted_layer.addTo(map);
-
 };
 
 // This function calculates the search area and
